f9e0f41dfd60f43b99a7c5bf4ff40c24ee78dc8a
gowtham A <gowthamarulmurugan2003@gmail.com>
2025-02-20 01:43:51 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
     <div className='app'>
       <Navbar setShowLogin={setShowLogin}/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/Agro-Foods' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/order' element={<PlaceOder/>}/>
       </Routes>
